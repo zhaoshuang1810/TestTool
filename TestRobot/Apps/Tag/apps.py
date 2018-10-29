af7ccc17254af990afea710236bf6dbfb79e4629
zhaoshuang@2018 <zhaoshuang>
2018-10-29 15:10:09 +0800

    d

--- TestRobot/Apps/Tag/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TagConfig(AppConfig):
-    name = 'Tag'
-    verbose_name = "测试用例标签"
--- TestRobot/Config/django.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-
-# mysql
-dbname = 'test_robot'
-dbusername = 'root'
-dbpassword = 'root1234'
-dbhost = 'localhost'
-dbport = 3306
-dbcharset = "utf8"
