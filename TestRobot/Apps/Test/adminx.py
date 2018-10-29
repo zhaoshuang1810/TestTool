f22f17d11c441e38227e0d18aed5ff1c6623830c
zhaoshuang@2018 <zhaoshuang>
2018-10-29 15:06:56 +0800

    delete

--- TestRobot/Apps/Test/adminx.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-import xadmin
-from xadmin import views
-from Config.project import projectName
-
--- TestRobot/Apps/Test/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestConfig(AppConfig):
-    name = 'Test'
-    verbose_name = "测试用例"
